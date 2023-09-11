9923f75bd8be6a74859d2946a7d4e916808725f8
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-11 11:23:45 +0300

    imort splash view

---
+++ lib/features/presentation/views/widgets/splsh_view_body.dart
@@ -0,0 +1,8 @@
+class SplshViewBody extends StatelessWidget {
+  const SplshViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
@@ -11,7 +12,7 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: SplashView(),
+      home: SplshView(),
     );
   }
 }
