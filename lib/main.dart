a868691fc185258758fa7199b977e2a98cd0c598
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-11 11:44:14 +0300

    create const color

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: 
+      ),
       home: SplshView(),
     );
   }
