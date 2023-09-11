90d7d0e42e96ce8b37c42fd9e65e0434661b007f
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-11 11:24:31 +0300

    created splsh view body

--- lib/features/presentation/views/splash_view.dart
+++ lib/features/presentation/views/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/presentation/views/widgets/splsh_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplshView extends StatelessWidget {
--- lib/features/presentation/views/widgets/splsh_view_body.dart
+++ lib/features/presentation/views/widgets/splsh_view_body.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/widgets.dart';
+
 class SplshViewBody extends StatelessWidget {
   const SplshViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column();
   }
 }
