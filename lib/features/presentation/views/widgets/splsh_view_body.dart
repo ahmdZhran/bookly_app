59a9c5627a80dbad9793323b700878c3ff6be0b9
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 14:12:43 +0300

    convert to statefull

--- lib/features/presentation/views/widgets/splsh_view_body.dart
+++ lib/features/presentation/views/widgets/splsh_view_body.dart
@@ -1,9 +1,14 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/widgets.dart';
 
-class SplshViewBody extends StatelessWidget {
+class SplshViewBody extends StatefulWidget {
   const SplshViewBody({super.key});
 
+  @override
+  State<SplshViewBody> createState() => _SplshViewBodyState();
+}
+
+class _SplshViewBodyState extends State<SplshViewBody> {
   @override
   Widget build(BuildContext context) {
     return Column(
