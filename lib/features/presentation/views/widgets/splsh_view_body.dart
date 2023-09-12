dd57e67aaec1a98e3d432d7e4f5f492e5ff83723
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 10:38:57 +0300

    add logo without background on splsh screen

--- lib/features/presentation/views/widgets/splsh_view_body.dart
+++ lib/features/presentation/views/widgets/splsh_view_body.dart
@@ -7,7 +7,7 @@ class SplshViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Image.asset(AssetsImages.logo2)],
+      children: [Image.asset(AssetsImages.logo3)],
     );
   }
 }
