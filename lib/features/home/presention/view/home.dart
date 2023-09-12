b7c84709b2e9a170ae57ff4a69736aacd5ffa82c
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 15:53:01 +0300

    created home view

---
+++ lib/features/home/presention/view/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HOme extends StatelessWidget {
+  const HOme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
