877f963e9f6ea0c054a7f22a891711d61e90db58
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 21:59:22 +0300

    adding best seller text

--- lib/features/home/presention/views/widgets/home_view_body.dart
+++ lib/features/home/presention/views/widgets/home_view_body.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:bookly_app/core/utils/text_style_mode.dart';
 import 'package:bookly_app/features/home/presention/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'feature_custom_list.dart';
@@ -7,11 +10,25 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeatureCustomList(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureCustomList(),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Text(
+              'Best seller',
+              style: Styles.titleMedium,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
