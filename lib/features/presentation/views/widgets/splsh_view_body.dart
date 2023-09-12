cdd53724df3007d3f914b61ffa5c85740c158099
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 14:54:39 +0300

    adding slidingText to file

--- lib/features/presentation/views/widgets/splsh_view_body.dart
+++ lib/features/presentation/views/widgets/splsh_view_body.dart
@@ -35,26 +35,3 @@ class _SplshViewBodyState extends State<SplshViewBody>
     );
   }
 }
-
-class SlidingText extends StatelessWidget {
-  const SlidingText({
-    super.key,
-    required this.slidingAnimation,
-  });
-
-  final Animation<Offset> slidingAnimation;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      // To make the ui build this only to improve performance
-      animation: slidingAnimation,
-      builder: (context, _) {
-        return SlideTransition(
-          position: slidingAnimation,
-          child: Text('Read free book'),
-        );
-      },
-    );
-  }
-}
