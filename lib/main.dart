3968117b23fe05d572bb9a45eb652deb5432e922
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 23:24:56 +0300

    change font theme for app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/constants.dart';
 import 'package:bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -14,7 +15,11 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratAlternatesTextTheme(
+            ThemeData.dark().textTheme),
+      ),
       home: SplshView(),
     );
   }
