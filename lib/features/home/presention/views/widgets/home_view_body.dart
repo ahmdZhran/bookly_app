7d852e41268c4e1524c8064046dfa83b9821c554
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-12 23:35:42 +0300

    created best book item

--- lib/features/home/presention/views/widgets/home_view_body.dart
+++ lib/features/home/presention/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@
 import 'package:bookly_app/core/utils/text_style_mode.dart';
 import 'package:bookly_app/features/home/presention/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets.dart';
 import 'feature_custom_list.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -26,8 +27,36 @@ class HomeViewBody extends StatelessWidget {
               'Best seller',
               style: Styles.titleMedium,
             ),
-          )
+          ),
+          BestSillerBooksList(),
         ],
+      ),
+    );
+  }
+}
+
+class BestSillerBooksList extends StatelessWidget {
+  const BestSillerBooksList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: SizedBox(
+        height: 140,
+        child: AspectRatio(
+          aspectRatio: 0.9 / 1,
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsImages.testImage,
+                  ),
+                )),
+          ),
+        ),
       ),
     );
   }
