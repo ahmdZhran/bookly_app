9d4605b31d9946f3575ff665c4db532d514dabc4
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-14 00:01:55 +0300

    naviage to details book view

--- lib/core/utils/app_rout.dart
+++ lib/core/utils/app_rout.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presention/views/details_book_view.dart';
 import 'package:bookly_app/features/home/presention/views/home_views.dart';
 import 'package:bookly_app/features/presentation/views/splash_view.dart';
 
@@ -5,6 +6,7 @@ import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/HomeView';
+  static const kBookDetailsView = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,6 +16,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
