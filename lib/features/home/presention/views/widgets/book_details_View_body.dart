b44f17591a654bd67d96a710246e9eafe1ec3975
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-14 00:11:19 +0300

    created book details app bar

--- lib/features/home/presention/views/widgets/book_details_View_body.dart
+++ lib/features/home/presention/views/widgets/book_details_View_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'custome_book_details_app_bar.dart';
+
 class BookDetailsBody extends StatelessWidget {
   const BookDetailsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            CustomBookDetailsAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
