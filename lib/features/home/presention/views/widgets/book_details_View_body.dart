a9b6ef3a99e3584febb0f5b5675ba769a2cf17b3
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-14 14:14:56 +0300

    add two texts

--- lib/features/home/presention/views/widgets/book_details_View_body.dart
+++ lib/features/home/presention/views/widgets/book_details_View_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/text_style_mode.dart';
 import 'package:bookly_app/features/home/presention/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
@@ -12,13 +13,32 @@ class BookDetailsBody extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(30),
+          padding: const EdgeInsets.all(30),
           child: Column(
             children: [
-              CustomBookDetailsAppBar(),
+              const CustomBookDetailsAppBar(),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: width * .2),
-                child: CustomBookImage(),
+                child: const CustomBookImage(),
+              ),
+              const SizedBox(
+                height: 43,
+              ),
+              const Text(
+                'The Jungle Book',
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 7,
+              ),
+              Opacity(
+                opacity: 0.7,
+                child: Text(
+                  'Rudyadrd Kibling',
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
               ),
             ],
           ),
