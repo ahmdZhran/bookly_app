19dbce6fb22240dbe3fa87f2de6f9c63e7bbaa89
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-14 16:11:14 +0300

    adding CustomeScrollView to be responsive screen

--- lib/features/home/presention/views/widgets/book_details_View_body.dart
+++ lib/features/home/presention/views/widgets/book_details_View_body.dart
@@ -15,61 +15,68 @@ class BookDetailsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            children: [
-              const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * .2),
-                child: const CustomBookImage(),
-              ),
-              const SizedBox(
-                height: 43,
-              ),
-              const Text(
-                'The Jungle Book',
-                style: Styles.textStyle30,
-              ),
-              const SizedBox(
-                height: 7,
-              ),
-              Opacity(
-                opacity: 0.7,
-                child: Text(
-                  'Rudyadrd Kibling',
-                  style: Styles.textStyle18.copyWith(
-                    fontStyle: FontStyle.italic,
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  const CustomBookDetailsAppBar(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * .2),
+                    child: const CustomBookImage(),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 7,
-              ),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const ActionButton(),
-              const SizedBox(
-                height: 50,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'You can also like this ',
-                  style: Styles.textStyle14.copyWith(
-                    fontWeight: FontWeight.w600,
+                  const SizedBox(
+                    height: 43,
                   ),
-                ),
+                  const Text(
+                    'The Jungle Book',
+                    style: Styles.textStyle30,
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      'Rudyadrd Kibling',
+                      style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const ActionButton(),
+                  const Expanded(
+                    child: SizedBox(
+                      height: 50,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'You can also like this ',
+                      style: Styles.textStyle14.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  const SimilarBooksListView(),
+                ],
               ),
-              const SimilarBooksListView(),
-            ],
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
