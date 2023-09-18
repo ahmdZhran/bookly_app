1e60ed6c2ae79de3c88c26f6aa9d171cb2660b09
Ahmed <ahmedmohameddx80@gmail.com>
2023-09-19 01:17:05 +0300

    implement fetch similar book

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/core/errors/failur.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
 }
